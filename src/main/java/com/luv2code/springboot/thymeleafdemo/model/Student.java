11edc7d4c5a02c1491eaf74c22183b35f2bdf896
Dmitry029 <kdp.029029@cmail.com>
2025-06-03 21:24:56 +0300

    add checkboxes

--- src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,4 +14,5 @@ public class Student {
     private String lastName;
     private String country;
     private String favoriteLanguage;
+    private List<String> favoriteSystems;
 }
